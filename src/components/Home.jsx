d5f3a240340f71a1cb622984c0caf7bf6b5a9dfe
chensov <senche.dev@gmail.com>
2023-11-03 01:24:47 +0000

    add BlogList component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,17 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-  const [name, setName] = useState("Arsen");
-  const [age, setAge] = useState(30);
-
-  const handleClick = () => {
-    setName("Sen");
-    setAge(25 + 1);
-  };
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum-1...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum-2...", author: "yoshi", id: 1 },
+    { title: "Web dev top tips", body: "lorem ipsum-3...", author: "mario", id: 3 },
+    { title: "Web dev top tips", body: "lorem ipsum-4...", author: "mario", id: 4 },
+  ]);
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>
-        {name} is {age} old.
-      </p>
-      <button onClick={handleClick}>Click Me</button>
+      <BlogList blogs={blogs} />
     </div>
   );
 };
